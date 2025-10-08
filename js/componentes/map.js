166f02356720ae59477820ea49483b7d6b8c48d5
wendelwaley3 <wendelwaley3@gmail.com>
2025-10-08 16:38:55 -0300

    Update map.js

--- js/componentes/map.js
+++ js/componentes/map.js
@@ -2,7 +2,5 @@ export function initMap(state) {
     state.map = L.map('mapid').setView([-15.7801, -47.9292], 5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 20, attribution: '&copy; OpenStreetMap' }).addTo(state.map);
     state.layers.lotes = L.featureGroup().addTo(state.map);
-    state.layers.app = L.featureGroup().addTo(state.map);
-    state.layers.risco = L.featureGroup().addTo(state.map);
-    state.layers.poligonais = L.featureGroup().addTo(state.map);
+    // ... (outras camadas)
 }
