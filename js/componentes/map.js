e78927706cfcbc2cae25c3a4d33858d88dae8b2c
wendelwaley3 <wendelwaley3@gmail.com>
2025-10-20 08:44:33 -0300

    Fix syntax error in initMap function

--- js/componentes/map.js
+++ js/componentes/map.js
@@ -2,5 +2,4 @@ export function initMap(state) {
     state.map = L.map('mapid').setView([-15.7801, -47.9292], 5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 20, attribution: '&copy; OpenStreetMap' }).addTo(state.map);
     state.layers.lotes = L.featureGroup().addTo(state.map);
-    // ... (outras camadas)
 }
